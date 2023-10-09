0c4f0003dd3ed7fcd67612e293723b3d7cd5388f
NY-lab <68333439+NY-lab@users.noreply.github.com>
2023-10-09 17:01:05 +0100

    Delete webpack.config.js

--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-module.exports = {
-  entry: './src/main.js', // Entry point for your JavaScript
-  output: {
-    filename: 'bundle.js', // Output JavaScript bundle name
-    path: path.resolve(__dirname, 'dist'), // Output directory
-  },
-};
